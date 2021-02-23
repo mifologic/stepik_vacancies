90000822f8c2446046011b91d054a32dfbc1d504
Mariana Piskareva <mifologic@gmail.com>
2021-02-23 22:34:17 +0300

    delete some unused code

--- vacancies/check_user_status.py
+++ vacancies/check_user_status.py
@@ -1,12 +1,2 @@
-from django.shortcuts import get_object_or_404
-
-from vacancies.models import Vacancy
-
-
 def get_current_user(request):
     return request.user if request.user.is_authenticated else None
-
-
-def get_vacancy(vacancy_id):
-    vacancy = get_object_or_404(Vacancy, pk=vacancy_id)
-    return vacancy
